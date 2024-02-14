ef717ff31f0b80a7526e456f8840fff73a63574e
Alexandru Ardelean <alex@shruggie.ro>
2024-02-14 10:06:25 +0200

    create_sd_card_image.sh: make sure the kernel name is Image
    Signed-off-by: Alexandru Ardelean <alex@shruggie.ro>

--- create_sd_card_image.sh
+++ create_sd_card_image.sh
@@ -107,7 +107,7 @@ mkdir -p $ROOTFS_DIR
 
 cp $(readlink -f ${BIN_DIR}/OpenCV_Bin.bin) $FAT_DIR/OpenCV_Bin.bin
 cp $(readlink -f ${BIN_DIR}/Codec_Bin.bin) $FAT_DIR/Codec_Bin.bin
-cp $(readlink -f $YOCTO_DEPLOY_DIR/Image) $FAT_DIR/
+cp $(readlink -f $YOCTO_DEPLOY_DIR/Image) $FAT_DIR/Image
 cp $(readlink -f $YOCTO_DEPLOY_DIR/r9a09g055ma3gbg-evaluation-board.dtb) $FAT_DIR/r9a09g055ma3gbg.dtb
 uncompress_rootfs_img_file "$ROOTFS_IMG_FILE" "$ROOTFS_DIR"
 
